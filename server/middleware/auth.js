66d1af058d140d84bb387b0e75a691f0def83efe
Tulasidhar <tulasidharmulakaluri>
2026-04-27 19:24:45 +0530

    removed unnecessary log

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,6 @@ const JWT_SECRET = process.env.JWT_SECRET || 'luminary_secret_dev';
 
 module.exports = (req, res, next) => {
   const authHeader = req.header('Authorization');
-  console.log("auth : " + authHeader);
   if (!authHeader) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
   const token = authHeader.split(' ')[1];
